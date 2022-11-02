0b633d062615f1e130f437f52668eb9995583056
lqhuyy <117263415+lqhuyy@users.noreply.github.com>
2022-11-02 15:37:24 +0700

    Add files via upload

---
+++ js/click show.js
@@ -0,0 +1,31 @@
+
+// sign in
+var modal = document.getElementById('id01');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+// end sign in
+
+
+// sign up
+var modal = document.getElementById('id02');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+// end sign in
+var modal = document.getElementById('id03');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
